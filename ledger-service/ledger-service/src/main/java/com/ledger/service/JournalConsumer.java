0158693c891898a003fb05a3395f0eed3adf3f8e
igmerwina <igmerwina@gmail.com>
2023-08-07 12:40:24 +0700

    [LEDGER][FIX] - fixing consumer for kafka

--- ledger-service/ledger-service/src/main/java/com/ledger/service/JournalConsumer.java
+++ ledger-service/ledger-service/src/main/java/com/ledger/service/JournalConsumer.java
@@ -1,10 +1,12 @@
 package com.ledger.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ledger.dto.request.JournalReq;
 import com.ledger.entity.JournalTrx;
 import com.ledger.repo.JournalTrxRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -17,11 +19,14 @@ public class JournalConsumer {
 
     @Autowired
     private final JournalTrxRepo journalTrxRepo;
+    private final ObjectMapper objectMapper;
 
     @Transactional
     @KafkaListener(topics = "${spring.kafka.consumer.topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumeJournalTrx(JournalReq req) throws Exception {
-        log.info("Start consumeJournalTrx - " + req.getIdOrder());
+    public void consumeJournalTrx(ConsumerRecord<String, String> message) throws Exception {
+        log.info("Start consumeJournalTrx : {} - {} - {}", message.key(), message.topic(), message.value());
+
+        JournalReq req = objectMapper.readValue(message.value(), JournalReq.class);
 
         JournalTrx journalTrx = JournalTrx.builder()
                 .id(System.currentTimeMillis())
